922953309f5133162eb29446ecf7ad4141d7aaf0
Journée Jérôme <jerome.journee@outlook.be>
2019-12-03 20:38:23 +0100

    :heavy_plus_sign:  Js Done With Bonus

--- js/main.js
+++ js/main.js
@@ -5,11 +5,15 @@
       document.querySelector('.sr-only').insertAdjacentHTML('afterend', `<ol id="fizzbuzz" class="grid">
         </ol>`);
       this.ol = document.querySelector('#fizzbuzz');
-      this.boucle(this.ol);
-      //window.addEventListener('scroll', this.scroll);
+      this.boucle(this.ol, 1);
     },
-    boucle(ol) {
-      for (this.chiffre = 0; this.chiffre <= 100; this.chiffre += 1) {
+    scroll(i){
+      if(window.pageYOffset + window.innerHeight === document.body.clientHeight){
+        this.boucle(this.ol,i);
+      }
+    },
+    boucle(ol, i = 1) {
+      for (this.chiffre = i; this.chiffre < (i + 100); this.chiffre += 1) {
         if (this.chiffre % 3 === 0 && this.chiffre % 5 === 0) {
           ol.insertAdjacentHTML('beforeend', '<li class="fizzbuzz">FI<i>zz</i>BU<i>zz</i></li>');
         } else if (this.chiffre % 5 === 0) {
@@ -20,25 +24,13 @@
           ol.insertAdjacentHTML('beforeend', `<li>${this.chiffre}</li>`);
         }
       }
+      console.log(window.pageYOffset + window.innerHeight);
+      console.log(document.body.clientHeight);
+      window.addEventListener('scroll', () => {
+        this.scroll(this.chiffre);
+      });
+
     },
-    // scroll() {
-    //   this.ol = document.querySelector('#fizzbuzz');
-    //   this.chiffreActu = this.chiffre + 10;
-    //
-    //   for (this.chiffre = 101; this.chiffre <= this.chiffreActu; this.chiffre += 1) {
-    //     if (this.chiffre % 3 === 0 && this.chiffre % 5 === 0) {
-    //       ol.insertAdjacentHTML('beforeend', '<li class="fizzbuzz">FI<i>zz</i>BU<i>zz</i></li>');
-    //     } else if (this.chiffre % 5 === 0) {
-    //       ol.insertAdjacentHTML('beforeend', '<li class="buzz">BU<i>zz</i></li>');
-    //     } else if (this.chiffre % 3 === 0) {
-    //       ol.insertAdjacentHTML('beforeend', '<li class="fizz">FI<i>zz</i></li>');
-    //     } else {
-    //       ol.insertAdjacentHTML('beforeend', `<li>${this.chiffre}</li>`);
-    //     }
-    //   }
-    //   this.chiffre = this.chiffre + 10;
-    //   this.chiffreActu = this.chiffreActu + 10;
-    // },
   };
   FizzBuzz.init();
 })()
